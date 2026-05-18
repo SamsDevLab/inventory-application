ba1c38f396a1f11809dd5f88afa3a0ca0602f08f
Sam Hadfield <sthtech@protonmail.com>
2026-05-18 16:42:13 -0500

    feat: add head partial, edit index view/controller
    Add head partial so that head would not have to be recreated within each
    view. Refactor index controller to not pass title object as this is now
    being dynamically passed to the head partial within index view. Refactor
    index view to include head partial

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const db = require("../db/queries");
 
 async function getHomepageData(req, res) {
   console.log("Retrieved all Homepage data");
-  res.render("index", { title: "Homepage" });
+  res.render("index");
 }
 
 module.exports = {

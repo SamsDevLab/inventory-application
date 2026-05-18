7ab001b4e25a6460cc4706f435bbe83947fd3404
Sam Hadfield <sthtech@protonmail.com>
2026-05-18 16:47:48 -0500

    refactor: pass title obj to head partial via views
    Replace hardcoded head in views files with head partial. Remove title obj
    from controllers in favor of passing it to the head partial through the
    views files

--- controllers/developers.js
+++ controllers/developers.js
@@ -3,7 +3,7 @@ const db = require("../db/queries");
 
 async function getAllDevelopers(req, res) {
   console.log("Retrieved all Developers");
-  res.render("developers", { title: "Developers" });
+  res.render("developers");
 }
 
 module.exports = {
--- controllers/genres.js
+++ controllers/genres.js
@@ -3,7 +3,7 @@ const db = require("../db/queries");
 
 async function getAllGenres(req, res) {
   console.log("Retrieved all Genres");
-  res.render("genres", { title: "Genres" });
+  res.render("genres");
 }
 
 module.exports = {

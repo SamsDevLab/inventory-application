32abc0c2fa5e8b7ce064f906caec77670bd9846d
Sam Hadfield <sthtech@protonmail.com>
2026-05-18 11:58:28 -0500

    feat: set path.join and view engine

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require("node:path");
 const { body, validationResult, matchedData } = require("express-validator");
 const indexRouter = require("./routes/index");
 const gamesRouter = require("./routes/games");
 const developersRouter = require("./routes/developers");
 const genresRouter = require("./routes/genres");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use("/", indexRouter);
 app.use("/games", gamesRouter);

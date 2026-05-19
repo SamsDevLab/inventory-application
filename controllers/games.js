2735219fef7c99a24ce8124555ec17974d8ddad0
Sam Hadfield <sthtech@protonmail.com>
2026-05-19 14:50:23 -0500

    feat: add 'new' view with route and controller fn
    Add 'new' view form to games so user can add a new game to the management
    app. Include a route for /games/new/ and a controller method which will
    add the game to the database

--- controllers/games.js
+++ controllers/games.js
@@ -5,6 +5,17 @@ async function getAllGames(req, res) {
   res.render("games/index", { title: "Games" });
 }
 
+function renderAddGameForm(req, res) {
+  res.render("games/new", { title: "Add New Game" });
+}
+
+async function addGameToDatabase(req, res) {
+  console.log(req.body);
+  res.redirect("/games/");
+}
+
 module.exports = {
   getAllGames,
+  renderAddGameForm,
+  addGameToDatabase,
 };
--- routes/games.js
+++ routes/games.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const gamesController = require("../controllers/games");
 
 router.get("/", gamesController.getAllGames);
+router.get("/new", gamesController.renderAddGameForm);
+router.post("/new", gamesController.addGameToDatabase);
 
 module.exports = router;

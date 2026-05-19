0bc051873a96577b21d883880e419dda65832846
Sam Hadfield <sthtech@protonmail.com>
2026-05-19 17:19:46 -0500

    feat: add routes/controllers – games' edit option
    Add edit option, which will eventually render individually in each game
    card. Add dynamic route and controllers to edit correct game

--- controllers/games.js
+++ controllers/games.js
@@ -14,8 +14,24 @@ async function addGameToDatabase(req, res) {
   res.redirect("/games/");
 }
 
+async function renderEditForm(req, res) {
+  // console.log(req.params.id);
+  // const queryResults = await db.queryGameById(req.params.id)
+  res.render("games/edit", { title: "Edit Game" });
+}
+
+async function addEditedGameToDatabase(req, res) {
+  console.log(req.params.id);
+  console.log(req.body);
+  // Use a query like this to overwrite existing data:
+  // await db.overWriteGameDetails
+  res.redirect("/games/");
+}
+
 module.exports = {
   getAllGames,
   renderAddGameForm,
   addGameToDatabase,
+  renderEditForm,
+  addEditedGameToDatabase,
 };
--- routes/games.js
+++ routes/games.js
@@ -4,7 +4,11 @@ const router = express.Router();
 const gamesController = require("../controllers/games");
 
 router.get("/", gamesController.getAllGames);
+
 router.get("/new", gamesController.renderAddGameForm);
 router.post("/new", gamesController.addGameToDatabase);
+
+router.get("/edit/:id", gamesController.renderEditForm);
+router.post("/edit/:id", gamesController.addEditedGameToDatabase);
 
 module.exports = router;

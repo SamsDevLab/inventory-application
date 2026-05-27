38d81b556fe32d684b5f96a91a47823f44020fa0
Sam Hadfield <sthtech@protonmail.com>
2026-05-27 09:57:17 -0500

    refactor: mv queries to games, delete queries file
    Move the remainder of the queries to the games model (addGameToGamesTable,
    queryGameId, addToGameDevelopersTable, addToGameGenresTable). Delete
    queries file as it's no longer needed

--- models/games.js
+++ models/games.js
@@ -43,4 +43,55 @@ async function getAllGamesWithDetails() {
   return gamesWithFullData;
 }
 
-module.exports = { getAllGamesWithDetails };
+async function addGameToGamesTable(newGameTitle) {
+  try {
+    await pool.query(`INSERT INTO games (game) VALUES ($1)`, [newGameTitle]);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function queryGameId(gameTitle) {
+  try {
+    console.log(gameTitle);
+    const result = await pool.query(
+      `SELECT games.id FROM games WHERE games.game = $1`,
+      [gameTitle],
+    );
+
+    const gameId = result.rows[0].id;
+    return gameId;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function addToGameDevelopersTable(gameId, developerId) {
+  try {
+    await pool.query(
+      `INSERT INTO game_developers (game_id, developer_id) VALUES($1, $2)`,
+      [gameId, developerId],
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function addToGameGenresTable(gameId, genreId) {
+  try {
+    await pool.query(
+      `INSERT INTO game_genres (game_id, genre_id) VALUES ($1, $2)`,
+      [gameId, genreId],
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = {
+  getAllGamesWithDetails,
+  addGameToGamesTable,
+  queryGameId,
+  addToGameDevelopersTable,
+  addToGameGenresTable,
+};
